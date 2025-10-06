9bc5f4e56d65b4bc5734f0d74c78e6dc846258e5
Ram-311 <mrramakrishna311@gmail.com>
2025-10-06 13:53:02 +0530

    changes server

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use(express.json({limit:"4mb"}));
 app.use(
   cors({
     origin: [
-      "https://chat-app-six-ashen-31.vercel.app/login", // ✅ your Vercel frontend domain
+      "https://chat-app-six-ashen-31.vercel.app", // ✅ your Vercel frontend domain
       "http://localhost:5173",                 // ✅ keep for local dev
     ],
     credentials: true,
