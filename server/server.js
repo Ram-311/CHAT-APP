c7faa9bf3d368d537615588279122c5b0cba0883
Ram-311 <mrramakrishna311@gmail.com>
2025-10-06 13:39:39 +0530

    changes server

--- server/server.js
+++ server/server.js
@@ -64,14 +64,14 @@ app.use('/api/messages',messageRouter);
 //connect to mongodb
 
 await connectDB();
-if(process.env.MODE_ENV!=="production"){
-    const PORT=process.env.PORT || 5000;
-
-    server.listen(PORT,()=>{
-        console.log(`server is running on port:  ${PORT}`)
-    })
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }  
      
-}
+
 
 export default server;
 
